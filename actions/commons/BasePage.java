bbde0fa3eeac73cc4a07f3097f90b772969e1b0d
hatt191 <hatt19191@gmail.com>
2021-04-07 08:16:42 +0700

    update BasePage (Topic 35)

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -1,5 +1,113 @@
 package commons;
 
-public class BasePage {
+import java.util.Set;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+	
+	public void openPageUrl(WebDriver driver, String pageUrl) {
+		driver.get(pageUrl);
+	}
+	
+	public String getPageTitle(WebDriver driver) {
+		return driver.getTitle();
+	}
+	
+	public String getPageUrl(WebDriver driver) {
+		return driver.getCurrentUrl();
+	}
+	
+	public String getPageSource(WebDriver driver) {
+		return driver.getPageSource();
+	}
+	
+	public Alert waitForAlertPresence(WebDriver driver) {
+		explicitWait = new WebDriverWait(driver, timeout);
+		return explicitWait.until(ExpectedConditions.alertIsPresent());	
+	}
+	
+	public void acceptAlert(WebDriver driver) {
+		alert = waitForAlertPresence(driver);
+		alert.accept();
+	}
+	
+	public void cancelAlert(WebDriver driver) {
+		alert = waitForAlertPresence(driver);
+		alert.dismiss();
+	}
+	
+	public void sendkeyToAlert(WebDriver driver, String value) {
+		alert = waitForAlertPresence(driver);
+		alert.sendKeys(value);
+	}
+	
+	public String getAlertText(WebDriver driver) {
+		alert = waitForAlertPresence(driver);
+		return alert.getText();
+	}
+	
+	public void switchToWindowByID(WebDriver driver, String parentID) {
+		Set<String> allWindowIDs = driver.getWindowHandles();
+		for (String windowID : allWindowIDs) {
+			if (!windowID.equals(parentID)) {
+				driver.switchTo().window(windowID);
+				break;
+			}
+		}
+	}
+
+	public void switchToWindowsByTitle(WebDriver driver, String expectedWindowTitle) {
+		Set<String> allWindowIDs = driver.getWindowHandles();
+		System.out.println("Total number of opening window-tabs: " + allWindowIDs.size());
+		for (String windowID : allWindowIDs) {
+			driver.switchTo().window(windowID);
+			String actualwindowTitle = driver.getTitle();
+			if (actualwindowTitle.equals(expectedWindowTitle)) {
+				break;
+			}
+		}
+	}
+
+	public void closeAllWindowsExceptParent(WebDriver driver, String parentID) {
+		Set<String> allWindowIDs = driver.getWindowHandles();
+		for (String windowID : allWindowIDs) {
+			if (!windowID.equals(parentID)) {
+				driver.switchTo().window(windowID);
+				driver.close();
+				sleepInSecond(1);
+			}
+			if (driver.getWindowHandles().size() == 1) {
+				driver.switchTo().window(parentID);
+				break;
+			}
+		}
+	}
+
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void backToPage(WebDriver driver) {
+		driver.navigate().back();
+	}
+	
+	public void refreshCurrentPage(WebDriver driver) {
+		driver.navigate().refresh();
+	}
+	
+	public void forwardToPage(WebDriver driver) {
+		driver.navigate().forward();
+	}
+	
+	private Alert alert;
+	private WebDriverWait explicitWait;
+	private long timeout = 30;
 }
